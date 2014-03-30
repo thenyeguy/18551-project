15720ae2e7e05272bb6ee13ab04428d0c6969a6c
Michael Ryan <michaeledryan@gmail.com>
2014-03-30 13:55:01 -0400

    Test UI app

---
+++ TestInterfaces/uis/src/main/java/edu/cmu/ece/ece551/uis/PentatonicScale.java
@@ -0,0 +1,25 @@
+package edu.cmu.ece.ece551.uis;
+
+/**
+ * Created by michaelryan on 3/28/14.
+ */
+public class PentatonicScale extends AbstractScale {
+
+    private static Interval[] majInts = {Interval.TONE, Interval.TONE,
+            Interval.THREETONES, Interval.TONE, Interval.THREETONES};
+    private static Interval[] minInts = {Interval.THREETONES,
+            Interval.TONE, Interval.TONE, Interval.THREETONES,
+            Interval.TONE};
+
+
+
+    public PentatonicScale(String startingNote, Tonality tonality) {
+        this.tonality = tonality;
+        this.tonic = startingNote;
+        if (tonality == Tonality.MINOR)
+            intervals = minInts;
+        else {
+            intervals = majInts;
+        }
+    }
+}
